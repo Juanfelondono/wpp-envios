4b11971b3a17db648a44fb5dbc42f9df745f0946
Juanfelondono <felipe.londono@utp.edu.co>
2023-06-16 23:26:30 -0500

    Verificacion si tiene Whatsapp

--- index.js
+++ index.js
@@ -13,13 +13,21 @@ client.on('ready', async () => {
   console.log('Cliente de WhatsApp está listo!');
 
   try {
-    const placas = ["XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D",]
-    const numbers = ['573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513',]; // Números de teléfono a los que se enviará el mensaje
-    const fechas = ["10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023",]
+    const placas = ["XZY27D", "XZY27D", "XZY27D",]
+
+    const fechas = ["26/07/2023", "27/07/2023", "28/07/2023",]
+
+    const numbers = ["573107215513", "573107215513", "57315513", ]; // Números de teléfono a los que se enviará el mensaje
+    
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     let index = 0
     for (const number of numbers) {
+      const isRegistered = await client.isRegisteredUser(number);
+      if (!isRegistered) {
+        console.log(`El número ${number} no tiene WhatsApp. mensaje numero ${index} de ${numbers.length-1}`);
+        continue; // Pasar al siguiente número si no tiene WhatsApp
+      }
       const message = `Buen dia! te habla *NICOL YULIANA* asesora del *CDA CIUDAD BONITA*, te informamos el vencimiento de tu *REVISION TECNICOMECANICA* DEL VEHICULO DE PLACA *${placas[index]} EL ${fechas[index]}*\n\n*Financiamos tu SOAT y Revisión*\n\n*Estamos ubicados en la Carrera 15 No. 11-56 Barrio Gaitán*\n\n*Nuestro horario de atención*\n\nLunes a Viernes de 6:30 am A 6:00 pm\nSabados: 6:30 am a 4:30 pm\nDomingos y Festivos: 8:00am a 12:00 m\n\nTe Esperamos`;
 
       await client.sendMessage(`${number}@c.us`, message); // Envío del mensaje
