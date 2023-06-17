ca134cc0f6b0bfdb8bd1f1437e8add8a72d3e430
Juanfelondono <felipe.londono@utp.edu.co>
2023-06-16 22:41:49 -0500

    Final

--- index.js
+++ index.js
@@ -1,67 +1,3 @@
-/*const { Client, NoAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-
-// equivalent to
-const client = new Client({
-    authStrategy: new NoAuth()
-});
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-});
-
-
-client.on('ready', () => {
-    console.log('Cliente de WhatsApp está listo!');
-  
-   /* // Escuchamos los mensajes entrantes
-    client.on('message', (message) => {
-      if (message.body === 'Hola') {
-        // Enviamos un mensaje de respuesta
-        client.sendMessage(message.from, 'Hola desde mi bot de WhatsApp!');
-      } else if (message.body === 'horario de atencion') {
-        // Enviamos un mensaje de respuesta
-        client.sendMessage(message.from, 'Nuestro horario de atención es de lunes a viernes, de 9:00 a.m. a 5:00 p.m.');
-      } else {
-        // Enviamos un mensaje de respuesta por defecto
-        client.sendMessage(message.from, 'No entendí lo que quieres decir. Por favor, envía "Hola" o "horario de atencion".');
-      }
-    }); 
-    client.sendMessage('+573167559457', 'Hola, este e sun ensayo de los envios automaticos');
-  });
-  
-client.initialize();*/
-/*'Buen día! le habla Stephanie asesora del CDA CALARCA, le informamos el vencimiento de su REVISION TECNICOMECANICA DEL VEHICULO DE PLACA HPA41E EL MARTES 21 DE FEBRERO\n\nVENTA DE SOAT, FINANCIAMOS SU REVISION TECNICOMECANICA\n\nEstamos ubicados frente al parador de camioneros, diagonal a la bascula Av. Centenario 6 200 Calarcá\n\nNuestro horario de atención \n\nLunes a Viernes de 6:30 am A 6:00 pm\nSábados: 6:30 am a 4:00 pm\nDomingos y Festivos: 7:30am a 12:30 pm\n\nLos esperamos!';*/
-
-// const { Client, NoAuth } = require('whatsapp-web.js');
-// const qrcode = require('qrcode-terminal');
-
-// const client = new Client({
-//     authStrategy: new NoAuth(),
-// });
-
-// client.on('qr', (qr) => {
-//     qrcode.generate(qr, {small: true});
-// });
-
-// client.on('ready', async () => {
-//   console.log('Cliente de WhatsApp está listo!');
-
-//   try {
-//     const message = 'Buen día! le habla Stephanie asesora del CDA CALARCA, le informamos el vencimiento de su REVISION TECNICOMECANICA DEL VEHICULO DE PLACA HPA41E EL MARTES 21 DE FEBRERO\n\nVENTA DE SOAT, FINANCIAMOS SU REVISION TECNICOMECANICA\n\nEstamos ubicados frente al parador de camioneros, diagonal a la bascula Av. Centenario 6 200 Calarcá\n\nNuestro horario de atención \n\nLunes a Viernes de 6:30 am A 6:00 pm\nSábados: 6:30 am a 4:00 pm\nDomingos y Festivos: 7:30am a 12:30 pm\n\nLos esperamos!';
-//     const numbers = ['573107215513', '573107215513', '57']; // Números de teléfono a los que se enviará el mensaje
-
-//     for (const number of numbers) {
-//       await client.sendMessage(`${number}@c.us`, message); // Envío del mensaje
-//       console.log(`Mensaje enviado correctamente al número ${number}`);
-//     }
-
-//   } catch (error) {
-//     console.error('Error al enviar el mensaje:', error);
-//   }
-// });
-
-// client.initialize();
 
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
@@ -77,15 +13,19 @@ client.on('ready', async () => {
   console.log('Cliente de WhatsApp está listo!');
 
   try {
-    const message = 'Buen día! Este es un mensaje automático enviado desde WhatsApp.';
-    const numbers = ['573107215513', '573107215513', '573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513','573107215513',]; // Números de teléfono a los que se enviará el mensaje
-
+    const placas = ["XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D","XZY27D",]
+    const numbers = ['573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513','573107215513', '573107215513', '573107215513',]; // Números de teléfono a los que se enviará el mensaje
+    const fechas = ["10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023","10-12-2023",]
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+    let index = 0
     for (const number of numbers) {
+      const message = `Buen dia! te habla *NICOL YULIANA* asesora del *CDA CIUDAD BONITA*, te informamos el vencimiento de tu *REVISION TECNICOMECANICA* DEL VEHICULO DE PLACA *${placas[index]} EL ${fechas[index]}*\n\n*Financiamos tu SOAT y Revisión*\n\n*Estamos ubicados en la Carrera 15 No. 11-56 Barrio Gaitán*\n\n*Nuestro horario de atención*\n\nLunes a Viernes de 6:30 am A 6:00 pm\nSabados: 6:30 am a 4:30 pm\nDomingos y Festivos: 8:00am a 12:00 m\n\nTe Esperamos`;
+
       await client.sendMessage(`${number}@c.us`, message); // Envío del mensaje
-      console.log(`Mensaje enviado correctamente al número ${number}`);
+      console.log(`Mensaje enviado correctamente al número ${number}, mensaje numero ${index} de ${numbers.length-1}`);
       await delay(2000); // Espera 2 segundos antes de enviar el siguiente mensaje
+      index++;
     }
 
     await client.logout(); // Cerrar sesión después de enviar los mensajes
